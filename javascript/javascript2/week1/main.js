6b34b66d5109bd6cce45f7bbcfbc57727db7a11f
Priyanka <Priyanka.mydesire@gmail.com>
2019-12-07 20:21:21 +0100

    Update homework files Js2 week1

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -62,33 +62,119 @@ animals=[
     "The Raven",
     "Ladybird beetle"]
 
+    spiritImg =[
+        "images/wolf.jpg",
+        "images/Butterfly.jpg",
+        "images/Spider.jpg",
+        "images/Owl.jpg",
+        "images/Hummingbird.jpg",
+        "images/Hawk.jpg",
+        "images/Grasshopper.jpg",
+        "images/Eagle.jpg",
+        "images/Raven.jpg",
+        "images/Ladybirdbeetle.jpg"        
+    ]
 
 function inputval()
 {//Benjamin - The fullmoon wolf.
     let name = document.getElementById("askbox").value;
-    let spirit = Math.floor(Math.random() * 10);
-    let content = `${name} - ${animals[spirit]}.`
-    
-    document.getElementById("show").innerHTML = content ;
-    document.getElementById('fogbg').style.display = 'block';
 
+    if(!name){
+       console.log(`Please enter a name, this can not be empty`)
+       document.getElementById("showmsz").innerHTML += "Hey! This field is blank. Let's write a name here.";
+       document.getElementById("fogbg").style.display = 'block';   
+       document.getElementById("showmsz").style.display = 'block';
+    }else{
+
+    let spirit = Math.floor(Math.random() * 10);
+    console.log(spirit)
+    let content = `${name} - ${animals[spirit]}.`
+    let imglink = `${spiritImg[spirit]}`
+    document.getElementById("show").innerHTML = content ;
+    document.getElementById("animalbox").style.backgroundImage = `URL(${imglink})`  ;
+    document.getElementById("fogbg").style.display = 'block';
+    document.getElementById("animalbox").style.display = 'block';
+    document.getElementById("show").style.display = 'block';
     console.log(content)
+    }
+    sessionStorage.clear();
 }
+
+
+
+// why it doesn't work but when i put code directly on the html page it works
+function close()
+{
+    document.getElementById('displaybox').style.display = 'none';
+    sessionStorage.clear();
+}
+
+
+
 
 function inputAnimal()
 {
     let animalName = document.getElementById("inputbox").value;
     if(!animalName){
-        console.log(`text box is empty, please write new animal name`);
+        
+
+        document.getElementById("showmsz").innerHTML += "Hey! You forgot to write a new animal name." ;
+        document.getElementById('fogbg').style.display = 'block';
+        document.getElementById(`animalbox`).style.display = 'none';
+        document.getElementById(`show`).style.display = 'none';
+        document.getElementById(`showmsz`).style.display = 'block';
+        sessionStorage.clear();
+        
     }else{
-    animals.push(animalName)
-    console.log(`New animal name ${animalName} is added to the spirit animal list`)
-    console.log(animals)
+        animals.push(animalName)
+        console.log(`New animal name ${animalName} is added to the spirit animal list`)
+        document.getElementById("fogbg").style.display = 'block';
+        // document.getElementById("animalbox").style.display = 'block';
+        document.getElementById("show").style.display = 'block';
+        document.getElementById("show").innerHTML += `New Spirit animal : ${animalName} is added to the spirit animal list`;
+
+        console.log(animals)
+        sessionStorage.clear();
 }
 
 }
 
 
+
+// Select options
+
+function getOption(){
+    let optionval = document.getElementById("options").value;
+    if (optionval == 1)
+    {
+        console.log("Hi you have selected the press button option")
+        console.log(optionval)
+        // document.getElementById(`optselection`).innerText = optionval;
+        // document.getElementById(`optselection`).innerHTML += optionval;
+        document.getElementById(`addanimal`).disabled = false;
+        document.getElementById(`addanimal`).className = "";
+        document.getElementById(`inputbox`).removeAttribute("onchange", "inputAnimal()");
+        sessionStorage.clear();
+        
+
+    }else if (optionval == 2){
+        console.log("Hi you have selected the hoverbox option")
+        console.log(optionval)
+        document.getElementById(`addanimal`).disabled = true;
+        document.getElementById(`addanimal`).className = "disable";
+        document.getElementById(`inputbox`).setAttribute("onchange", "inputAnimal()");
+        sessionStorage.clear(); 
+        // onchange="myFunction()
+        // inputAnimal();
+    }
+
+}
+//     let askboxval = document.getElementById("askbox");
+// statement.push(askboxval.options[askboxval.selectedIndex].value);
+
+
+// onchange="myFunction()" // if user is using keyboard and press enter or with mouse clicks outside
+// onmouseup="mUp(this)" // if user is using mouse and after pressing to click when she relese the key
 
 // A user select that she only wants to generate a spirit animal when the input is hovered.
 

e808be93a6e5d9e560afa4552c5d182a19e107db
priyankapandey13 <55982058+priyankapandey13@users.noreply.github.com>
2020-03-03 17:20:49 +0100

    Updating warmup index.js
    Minor changes

--- nodejs/week2/warmup/index.js
+++ nodejs/week2/warmup/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express()
-
+const PORT = 4000;
 
 
 
 
 app.get('/',(req, res, next)=>{
     res.send("Hi index page");
+    next();
 })
 
 // /search?q=priyanka
 
 app.get('/search', (req, res, next)=>{
     res.send(`You have searched for ${req.query.q}`);
+    next();
 })
-
 
 
 
@@ -22,21 +23,20 @@ app.get('/search', (req, res, next)=>{
 
 // GET /numbers/add?first=<number here>&second=<number here>. In response send sum (first + second).
 
-
-app.get('/numbers/add?first=:first&second=:second', function (req, res) {
+app.get('/numbers/add', function (req, res) {
     response = {
       result: parseInt(req.query.first) + parseInt(req.query.second),
    };
    res.send(response);
 })
-// localhost:4000/numbers/add?first=2&second=5
+// localhost:4000/numbers/add?first=2&second=5  => "result": 7
 
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ exercise 1 part2
 
 // GET /numbers/multiply/<first number here>/<second number here>. in response send multiplication (first * second).
 
-// localhost:4000/numbers/multiply/2/5
+// localhost:4000/numbers/multiply/2/5  => "result": 10
 app.get('/numbers/multiply/:first/:second', function (req, res) {
     response = {
       result: parseInt(req.params.first) * parseInt(req.params.second),
@@ -46,10 +46,6 @@ app.get('/numbers/multiply/:first/:second', function (req, res) {
 
 
 
-
-
-
-
-app.listen(4000,()=>{
-    console.log('port is running');
+app.listen(PORT,()=>{
+    console.log('${PORT} Port is running');
 })

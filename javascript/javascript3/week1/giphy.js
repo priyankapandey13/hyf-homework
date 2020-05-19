f53becd81ab2984c2a7e60f83426b996a064c57e
Priyanka <Priyanka.mydesire@gmail.com>
2020-05-19 14:48:10 +0200

    Updated ricardo's feedbacks

--- javascript/javascript3/week1/giphy.js
+++ javascript/javascript3/week1/giphy.js
@@ -8,21 +8,23 @@ let searchbtn = document.getElementById("srchbtn");
 
 let datacallback = (data1) => {
   for (let i = 0; i < data1.pagination.count; i++) {
-    document.getElementById(
-      "alldata"
-    ).innerHTML += ` <img src="${data1.data[i].images.downsized_large.url}">`;
+    let img = document.createElement("IMG");
+      img.setAttribute("src", data1.data[i].images.downsized_large.url);
+      document.getElementById("alldata").appendChild(img);
     // Get a random dog image and display it in the browser
   }
 };
 
 function getdata() {
-  const keyword = document.getElementById("searchbox").value;
+  let keyword = document.getElementById("searchbox").value;
   const gifcount = document.getElementById("gifnumber").value;
   // created an inputbox and a button for searching the gif file
 
   if (!keyword) {
-    alert("This can't be blank");
+    document.getElementById("searchbox").style.borderColor = "red";
+
   } else {
+    document.getElementById("searchbox").style.borderColor = "#d2d6d9";
     fetch(
       `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${gifcount}`
     )
